412c2160e38bf27dcddec405a2ac9a4b3115fbfe
thecoons <antobarth@gmail.com>
2016-06-20 12:02:36 +0200

    update touch

--- HearthDeepClient/clientHeathDeep.py
+++ HearthDeepClient/clientHeathDeep.py
@@ -77,8 +77,9 @@ class HearthDeepClientScreen(Screen):
         for line in gen:
             last_touch = os.path.getmtime(self.logs_path+line)
             current_time = time.time()
-            Logger.info('CHDCMenu: '+line+' '+str(last_touch)+' '+str(current_time)+' '+str(current_time - last_touch))
-            if(last_touch > 60000):
+            inter_time = current_time - last_touch
+            Logger.info('CHDCMenu: '+line+' '+str(last_touch)+' '+str(current_time)+' '+str(inter_time))
+            if(inter_time > 60000):
                 self.send_log(line)
                 self.list_upload.append(line)
 
